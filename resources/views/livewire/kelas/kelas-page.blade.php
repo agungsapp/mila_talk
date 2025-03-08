3afbe62c54ab4fd084ab1def3081e75ed7348ef4
agungsapp <agung.dni19@gmail.com>
2025-03-09 03:14:04 +0700

    integrasi sb admin panel

---
+++ resources/views/livewire/kelas/kelas-page.blade.php
@@ -0,0 +1,30 @@
+<div>
+		<div class="row">
+
+				<div class="col-12">
+						<div class="card mb-4 shadow">
+								<div class="card-header py-3">
+										<h6 class="font-weight-bold text-primary m-0">Buat kelas baru</h6>
+								</div>
+								<div class="card-body">
+										<form>
+												<div class="form-group">
+														<label for="exampleInputEmail1">Email address</label>
+														<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+														<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+												</div>
+												<div class="form-group">
+														<label for="exampleInputPassword1">Password</label>
+														<input type="password" class="form-control" id="exampleInputPassword1">
+												</div>
+												<div class="form-group form-check">
+														<input type="checkbox" class="form-check-input" id="exampleCheck1">
+														<label class="form-check-label" for="exampleCheck1">Check me out</label>
+												</div>
+												<button type="submit" class="btn btn-primary">Submit</button>
+										</form>
+								</div>
+						</div>
+				</div>
+		</div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Livewire\Dashboard\DashboardPage;
+use App\Livewire\Kelas\KelasPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
 });
+
+
+// Route::group(function () {});
+Route::get('dashboard', DashboardPage::class)->name('dashboard');
+Route::get('kelas', KelasPage::class)->name('kelas');
